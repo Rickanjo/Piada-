099dd84eb612b7c1af09ae24aca98b88e07bb8c5
Rick Arcanjo <Ricardo.cassettari07@gmail.com>
2023-03-10 14:59:24 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿ 
+Console.WriteLine("Piada");
+Console.WriteLine();
+
+Console.WriteLine("Duas formigas se encontraram e pararam para conversar:");
+Console.WriteLine("- Oi, qual é o seu nome?");
+Console.WriteLine("- Fu.");
+Console.WriteLine("- Fu o quê?");
+Console.WriteLine("- Fu Miga!");
+
+
